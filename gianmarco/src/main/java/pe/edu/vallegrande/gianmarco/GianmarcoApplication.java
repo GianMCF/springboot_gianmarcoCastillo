e2d2bccf177f15db94f84195fd694638e424f7cc
GianmarcoCastilloFlores <gianmarco.castillo@vallegrande.edu.pe>
2025-08-22 17:17:40 +0000

    feat: add project files

---
+++ gianmarco/src/main/java/pe/edu/vallegrande/gianmarco/GianmarcoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.gianmarco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GianmarcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GianmarcoApplication.class, args);
+	}
+
+}
---
+++ gianmarco/src/test/java/pe/edu/vallegrande/gianmarco/GianmarcoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.gianmarco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GianmarcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
